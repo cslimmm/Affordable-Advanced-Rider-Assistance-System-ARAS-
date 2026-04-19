3e6352ba1c37dce6186c6171ede7f3c10bea1a3a
ET0506 <esthertong2004@gmail.com>
2026-04-19 15:24:39 +0800

    Delete wifi_service.h

--- wifi_service.h
+++
@@ -1,6 +0,0 @@
-//tells the compiler to include this header file only one time, even if it is included many times in the project.
-#pragma once 
-
-//bool wifiIsConnected();
-void wifiInit();
-void wifiLoop();
